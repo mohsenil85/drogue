3da7d981c1e7276c17c8e5b4276c9029bf75e8ce
mohsenil85 <mohsenil85@gmail.com>
2016-01-06 21:51:02 -0800

    new

--- drogue.lisp
+++ drogue.lisp
@@ -5,3 +5,16 @@
   (:export :main))
 
 (in-package #:drogue)
+
+;;min window size ensurer
+;;defui
+;;;keymaps
+;;;callbacks
+;;;;start with inventory ui
+;;;;also world state object
+
+
+
+(defun main (args)
+  (declare (ignore args))
+  (format t "lol~%"))
